3572a398e96987a0babcacaf62ba178abd224d72
Evan Barry <evan.barry@students.ittralee.ie>
2020-10-06 12:39:30 +0100

    First Commit
    All work up to now was done before repo was set up

---
+++ assignment1/Assets/Sphere_script.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_script : MonoBehaviour
+{
+    public Vector3 velocity  = new Vector3(0,0,0);
+    Vector3 accelleration = new Vector3(0, -9.8f, 0);
+    public float Radius_of_sphere = 0.5f;
+    private float Coefficient_of_Restitution = 0.6f;
+    public Plane_script plane;
+
+    public objectManagerScript objectManager;
+    public Sphere_script[] spheres;
+    public Sphere_script nearestSphere;
+
+    public Vector3 sphereNormal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = FindObjectOfType<Plane_script>();
+        objectManager = FindObjectOfType<objectManagerScript>();
+        spheres = objectManager.GetSpheres();
+        foreach (Sphere_script sphere in spheres)
+        {
+            if(Vector3.Distance(sphere.transform.position, transform.position) > 1)
+            {
+                nearestSphere = sphere;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float d1 = plane.distance_to(transform.position) - Radius_of_sphere;
+        float distance_to_nearest_sphere = Vector3.Distance(transform.position, nearestSphere.transform.position);
+
+        velocity += accelleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        
+        float distance_from_centre_to_plane = plane.distance_to(transform.position);
+        float d2 = distance_from_centre_to_plane - Radius_of_sphere;
+
+        if (d2 <= 0)
+        {
+            //Debug.Log("D2 <= 0");
+            Vector3 para = parallel_component(velocity, plane.normal);
+            Vector3 perp = perpendicular_component(velocity, plane.normal);
+
+            float time_of_impact = Time.deltaTime * (d1 / (d1 - d2)); 
+            transform.position -= velocity * (Time.deltaTime - time_of_impact);
+
+            velocity = perp - (Coefficient_of_Restitution * para);
+
+            transform.position += velocity * (Time.deltaTime - time_of_impact);
+
+            // float overlap = Radius_of_sphere - distance_from_centre_to_plane;
+            // transform.position += overlap * plane.normal;
+        }
+
+        // float R = Radius_of_sphere;
+        // float r = nearestSphere.Radius_of_sphere;
+        // float d = Vector3.Distance(transform.position, nearestSphere.transform.position);
+        // sphereNormal = (nearestSphere.transform.position - transform.position).normalized;
+
+        // if(d <= R + r)
+        // {
+        //     Debug.Log("Spheres Colliding");
+        //     Vector3 pointOfImpact = transform.position + (R * sphereNormal);
+
+            //Vector3 newPara = (((sphereNormal - nearestSphere.sphereNormal)/(sphereNormal + nearestSphere.sphereNormal)) * parallel_component(velocity, sphereNormal)) + (((2 * nearestSphere.sphereNormal)/(sphereNormal + nearestSphere.sphereNormal)) * parallel_component(nearestSphere.velocity, nearestSphere.sphereNormal));
+
+            //velocity = perpendicular_component(velocity, sphereNormal) + (Coefficient_of_Restitution * newPara);
+
+            //transform.position += velocity * Time.deltaTime;
+
+        //}
+    }
+    public Vector3 perpendicular_component(Vector3 vel, Vector3 norm)
+    {
+        return vel - parallel_component(vel, norm);
+    }
+    public Vector3 parallel_component(Vector3 vel, Vector3 norm)
+    {
+        return Vector3.Dot(vel, norm) * norm;
+    }
+
+    public void setNearestPlane(Plane_script p)
+    {
+        if(p.distance_to(transform.position) < plane.distance_to(transform.position))
+        {
+            plane = p;
+        }
+    }
+
+    public void setNearestSphere()
+    {
+        foreach (Sphere_script sphere in spheres)
+        {
+            if(Vector3.Distance(sphere.transform.position, transform.position) > 0)
+            {
+                if(Vector3.Distance(sphere.transform.position, transform.position) < Vector3.Distance(nearestSphere.transform.position, transform.position))
+                {
+                    nearestSphere = sphere;
+                }
+                
+            }
+        }
+    }
+}
---
+++ assignment1/Assets/objectManagerScript.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectManagerScript : MonoBehaviour
+{
+
+    public Plane_script[] planes = new Plane_script[5];
+    public Sphere_script[] spheres = new Sphere_script[2];
+
+    public Sphere_script s1;
+    public Sphere_script s2;
+    public float R;
+    public float r;
+    public float d;
+    public Vector3 Point_of_impact;
+    public Vector3 s1Normal;
+    public Vector3 s2Normal;
+    public Vector3 s1Para;
+    public Vector3 s1Perp;
+    public Vector3 s2Para;
+    public Vector3 s2Perp;
+    private float Coefficient_of_Restitution = 0.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planes[0].define_plane(new Vector3(0, -2, 0), new Vector3(-0.02f, 1, 0.02f));
+        planes[1].define_plane(new Vector3(5, 0, 0), new Vector3(-1, 0, 0));
+        planes[2].define_plane(new Vector3(-5, 0, 0), new Vector3(1, 0, 0));
+        planes[3].define_plane(new Vector3(0, 0, 5), new Vector3(0, 0, -1));
+        planes[4].define_plane(new Vector3(0, 0, -5), new Vector3(0, 0, 1));
+
+        s1 = spheres[0];
+        s2 = spheres[1];
+
+        R = s1.Radius_of_sphere;
+        r = s2.Radius_of_sphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Sphere_script sphere in spheres)
+        {
+            foreach (Plane_script plane in planes)
+            {
+                sphere.setNearestPlane(plane);
+            }
+
+            sphere.setNearestSphere();
+        }
+
+        d = Vector3.Distance(s1.transform.position, s2.transform.position);
+
+        if(d <= R + r)
+        {
+            Debug.Log("Spheres Colliding!");
+
+            s1Normal = (s2.transform.position - s1.transform.position).normalized;
+            s2Normal = -s1Normal;
+
+            Point_of_impact = s1.transform.position + (R * s1Normal);
+
+            s1Para = s1.parallel_component(s1.velocity, s1Normal);
+            s1Perp = s1.perpendicular_component(s1.velocity, s1Normal);
+
+            s2Para = s2.parallel_component(s2.velocity, s1Normal);
+            s2Perp = s2.perpendicular_component(s1.velocity, s1Normal);
+
+            s1.transform.position -= s1.velocity * Time.deltaTime;
+            s2.transform.position -= s1.velocity * Time.deltaTime;
+
+            Vector3 newV1Para = newV1Parallel(s1Normal, s2Normal, s1Para, s2Para);
+            s1.velocity = s1Perp - (Coefficient_of_Restitution * newV1Para);
+
+            Vector3 newV2Para = newV2Parallel(s1Normal, s2Normal, s1Para, s2Para);
+            s2.velocity = s2Perp - (Coefficient_of_Restitution * newV2Para);
+
+            s1.transform.position += s1.velocity * Time.deltaTime;
+            s2.transform.position += s2.velocity * Time.deltaTime;
+
+        }
+
+    }
+
+    public Sphere_script[] GetSpheres()
+    {
+        return spheres;
+    }
+
+    public Vector3 newV1Parallel(Vector3 m1, Vector3 m2, Vector3 oldV1Para, Vector3 oldV2Para)
+    {
+        //Formula - (((m1-m2)/(m1+m2)*oldV1Para) + ((2*m2)/(m1+m2))*oldV2Para)
+
+        Vector3 m1Minusm2 = m1 - m2;
+        Vector3 m1Plusm2 = m1 + m2;
+        Vector3 m2Times2 = m2 * 2;
+
+        Vector3 firstDivision = new Vector3(m1Minusm2.x/m1Plusm2.x, m1Minusm2.y/m1Plusm2.y, m1Minusm2.z/m1Plusm2.z);
+        Vector3 secondDivision = new Vector3(m2Times2.x/m1Plusm2.x, m2Times2.y/m1Plusm2.y, m2Times2.z/m1Plusm2.z);
+
+        return Vector3.Scale(firstDivision, oldV1Para) + Vector3.Scale(secondDivision, oldV2Para);
+        
+    }
+
+    public Vector3 newV2Parallel(Vector3 m1, Vector3 m2, Vector3 oldV1Para, Vector3 oldV2Para)
+    {
+        //Formula - (((m2-m1)/(m1+m2)*oldV2Para) + ((2*m1)/(m1+m2))*oldV1Para)
+
+        Vector3 m2Minusm1 = m2 - m1;
+        Vector3 m1Plusm2 = m1 + m2;
+        Vector3 m1Times2 = m1 * 2;
+
+        Vector3 firstDivision = new Vector3(m2Minusm1.x/m1Plusm2.x, m2Minusm1.y/m1Plusm2.y, m2Minusm1.z/m1Plusm2.z);
+        Vector3 secondDivision = new Vector3(m1Times2.x/m1Plusm2.x, m1Times2.y/m1Plusm2.y, m1Times2.z/m1Plusm2.z);
+
+        return Vector3.Scale(firstDivision, oldV2Para) + Vector3.Scale(secondDivision, oldV1Para);
+    }
+}
